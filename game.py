1cdb681ddb0413f24a9981cba858b282663ab2c6
Piri <99538413+PiragashSelva@users.noreply.github.com>
2024-02-14 16:42:40 +0000

    Cannot play played moves
    Stops user from being able write over a played square

--- game.py
+++ game.py
@@ -3,12 +3,16 @@ def display_board(board): #function to display the board at the current point in
     print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + "\n---+---+---")
     print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2])
 
-def player_input(Player_Symbol): #function to get player to enter their move
+def player_input(Player_Symbol, played_moves): #function to get player to enter their move
     while True:
         try:
             move = int(input("Player "+ Player_Symbol +". Enter your move (1-9): "))
             if 1<=move<=9:
-                return move
+                if move not in played_moves: #check if move has already been played
+                    played_moves.append(move) # add move to list of moves that have been played
+                    return move 
+                else:
+                    print("This move has already been played. Try again.")
             else:
                 print("Invalid input. Please enter a number between 1 and 9")
         except:
@@ -19,10 +23,11 @@ def play_tic_tac_toe(): #function to play the game
     Players = ["X", "O"]
     Current_player = 0
     board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    played_moves = []
 
     while True:
         display_board(board)
-        move = player_input(Players[Current_player]) #obtaining move from player
+        move = player_input(Players[Current_player], played_moves) #obtaining move from player
 
         ################adding move to board################
         if move <= 3:
